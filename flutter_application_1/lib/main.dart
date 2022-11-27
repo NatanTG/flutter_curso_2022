a60c7b134235015c4a38bfdb29ae687fd7f837b3
natan <natatidioligirotto@gmail.com>
2022-11-27 00:19:28 -0300

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+        child: Text('Flutterando'),
+      ),
+    );
+  }
+}
